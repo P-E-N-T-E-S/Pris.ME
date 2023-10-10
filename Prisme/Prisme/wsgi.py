870052c14b8d5b8c361c8283d54702237be2f417
evaldocunhaf <evaldocunhaf@gmail.com>
2023-10-10 19:55:09 -0300

    refactoring do nome do projeto

--- Prisme/Prisme/wsgi.py
+++ Prisme/Prisme/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AljavaP project.
+WSGI config for Prisme project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AljavaP.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Prisme.settings")
 
 application = get_wsgi_application()
--- Prisme/prisme_app/apps.py
+++ Prisme/prisme_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AljavaConfig(AppConfig):
+class PrismeAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "Aljava"
+    name = "prisme_app"
