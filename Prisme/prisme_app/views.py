067191b8487dfb72aaab0bea76890296a9c99986
Luiz Eduardo Araújo Brayner <Brayner.luizeduardo@Gmail.com>
2023-10-12 12:36:58 -0300

    Implementações básicas: pasta de templates,view e htmls básicos adicionados, urls do app e alguns ajustes no settings.py

--- Prisme/prisme_app/views.py
+++ Prisme/prisme_app/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 from .utils import linhas
 # Create your views here.
-'''exemplo de gráfico
-def view(request):
-    query = Modelo.objects.all
-    x = [x.atributo for x in query]
-    y = [y.atributo for y in query]
-    grafico = linhas(x, y, titulo="título", tit_x="titulo col x", tit_y = "titulo col y)
-    return(request, "arquivo.html", {"graf_linha": grafico}
-    
-exemplo html
-<img src=data:image/png;base64, {{ graf_linha safe }}
-'''
+
+def home(request):
+    contexto = {
+    }
+    return render(request, "home.html", context=contexto)
