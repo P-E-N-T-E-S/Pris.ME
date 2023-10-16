d61f643f87f9c8ad0a37cf10304d164867dad866
evaldocunhaf <evaldocunhaf@gmail.com>
2023-10-15 23:37:38 -0300

    ajustes nos urls dinâmicos

--- Prisme/prisme_app/urls.py
+++ Prisme/prisme_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns =[
     path('logout/', views.Logout, name='logout'),
     path("add_projeto",views.add_projeto, name="add_projeto"),
     path("add_dados",views.add_dados, name="add_dados"),
-    path("add_linhas",views.add_linhas, name="add_linhas"),
+    path("add_linhas/<str:dado_impacto_id>",views.add_linhas, name="add_linhas"),
     path("visualizar_projetos",views.visualizar_projetos, name="visualizar_projetos"),
-    path("editar_linha_impacto",views.editar_linha_impacto, name="editar_linha_impacto"),
-    path("detalhes_dado",views.visualizar_linhas_impacto, name="detalhes_dado"),
+    path("editar_linha_impacto/<str:dado_impacto_id>",views.editar_linha_impacto, name="editar_linha_impacto"),
+    path("detalhes_dado/<str:dado_impacto_id>",views.visualizar_linhas_impacto, name="detalhes_dado"),
 ]
