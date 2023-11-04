ab74c7c1acfea46ebfe35fea2510a3fc28324a9a
Thomazrlima <thomazlima211@gmail.com>
2023-11-04 17:03:55 -0300

    Editar layout

--- Prisme/prisme_app/urls.py
+++ Prisme/prisme_app/urls.py
@@ -15,5 +15,6 @@ urlpatterns =[
     path("detalhes_dado/<str:dado_impacto_id>",views.visualizar_linhas_impacto, name="detalhes_dado"),
     path('editar_projeto/<int:projeto_id>/', views.editar_projeto, name='editar_projeto'),
     path('editar_dado/<int:dado_impacto_id>/', views.editar_dado, name='editar_dado'),
+    path('editar_estilo', views.editar_estilo, name='editar_estilo'),
 ]
 
